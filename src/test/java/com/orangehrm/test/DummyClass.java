3ab1cb67892496a97ee1b50adfd641f88e6c0595
Samir Jadhav <jadhavsamir644@gmail.com>
2026-01-03 19:26:13 +0530

    added test

--- src/test/java/com/orangehrm/test/DummyClass.java
+++ src/test/java/com/orangehrm/test/DummyClass.java
@@ -1,17 +1,32 @@
 package com.orangehrm.test;
 
 import org.testng.Assert;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
 import org.testng.annotations.Test;
 
 import com.orangehrm.base.BaseClass;
 
 public class DummyClass extends BaseClass {
 	
+	
+	
 	@Test
 	void testURL() {
 		
 		String pageTitle = driver.getTitle();
 		Assert.assertEquals(pageTitle, "OrangeHRM");
+		
+		given()
+		
+		.when()
+			.get(prop.getProperty("url"))
+			
+		.then()
+			.statusCode(200);
+		
+		
 	}
 
 }
